fdd0d5bd5d2afecacd7d2c5f3ab76fc46a5e52a7
Daniel Clough <danielleeclough86@hotmail.co.uk>
2022-04-23 12:54:16 +0100

    complete redux toolkit shopping cart

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -4,6 +4,7 @@ import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { mobile } from "../responsive";
+import { useSelector } from "react-redux";
 
 const Container = styled.div``;
 
@@ -153,6 +154,7 @@ const Button = styled.button`
 `;
 
 const Cart = () => {
+  const cart = useSelector(state=>state.cart)
   return (
     <Container>
       <Navbar />
@@ -169,57 +171,37 @@ const Cart = () => {
         </Top>
         <Bottom>
           <Info>
-            <Product>
-              <ProductDetail>
-                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
-                <Details>
-                  <ProductName>
-                    <b>Product:</b> AKIRA RUNNING SHOES
-                  </ProductName>
-                  <ProductId>
-                    <b>ID:</b> 93813718293
-                  </ProductId>
-                  <ProductColor color="black" />
-                  <ProductSize>
-                    <b>Size:</b> 37.5
-                  </ProductSize>
-                </Details>
-              </ProductDetail>
-              <PriceDetail>
-                <ProductAmountContainer>
-                 <Remove />
-                  <ProductAmount>2</ProductAmount>
-                  <Add />
-                </ProductAmountContainer>
-                <ProductPrice>£ 29.99</ProductPrice>
-              </PriceDetail>
-            </Product>
-            <Hr />
-            <Product>
-              <ProductDetail>
-                <Image src="https://i.pinimg.com/originals/2d/af/f8/2daff8e0823e51dd752704a47d5b795c.png" />
-                <Details>
-                  <ProductName>
-                    <b>Product:</b> BJORK T-SHIRT
-                  </ProductName>
-                  <ProductId>
-                    <b>ID:</b> 93813718293
-                  </ProductId>
-                  <ProductColor color="gray" />
-                  <ProductSize>
-                    <b>Size:</b> M
-                  </ProductSize>
-                </Details>
-              </ProductDetail>
-              <PriceDetail>
-                <ProductAmountContainer>
-                  <Remove />
-                  <ProductAmount>1</ProductAmount>
-                  <Add />
-                </ProductAmountContainer>
-                <ProductPrice>£ 12.99</ProductPrice>
-              </PriceDetail>
-            </Product>
+
+            { cart.products.map((product) => (
+                 <Product>
+                 <ProductDetail>
+                   <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
+                   <Details>
+                     <ProductName>
+                       <b>Product:</b> {product.title}
+                     </ProductName>
+                     <ProductId>
+                       <b>ID:</b> {product._id}
+                     </ProductId>
+                     <ProductColor color={product.color} />
+                     <ProductSize>
+                       <b>Size:</b> {product.size}
+                     </ProductSize>
+                   </Details>
+                 </ProductDetail>
+                 <PriceDetail>
+                   <ProductAmountContainer>
+                    <Remove />
+                     <ProductAmount>2</ProductAmount>
+                     <Add />
+                   </ProductAmountContainer>
+                   <ProductPrice>£ 29.99</ProductPrice>
+                 </PriceDetail>
+                 <Hr />
+               </Product>
+            ))
+            };
+
           </Info>
           <Summary>
             <SummaryTitle>ORDER SUMMARY</SummaryTitle>
--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -8,6 +8,8 @@ import { Add, Remove } from "@material-ui/icons"
 import { mobile } from "../responsive";
 import { useEffect, useState } from "react"
 import { publicRequest} from "../requestMethods"
+import { addProduct } from "../redux/cartRedux"
+import { useDispatch } from "react-redux"
 
 
 const Container = styled.div`
@@ -122,6 +124,7 @@ const Product = () => {
   const [quantity, setQuantity] = useState(1);
   const [color, setColor] = useState("");
   const [size, setSize] = useState("");
+  const dispatch = useDispatch();
 
 
 useEffect(()=>{
@@ -143,9 +146,10 @@ const handleQuantity = (type) => {
 };
 
 const handleClick = () => {
-  //update cart
-  
-}
+  dispatch(
+    addProduct({ ...product, quantity, color, size})
+    );
+};
 
   return (
 <Container>
